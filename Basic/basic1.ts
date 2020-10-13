b42ffbc006a729d558446cb0cae7e6061955aa63
Dominik Ringbauer <dominik.ringbauer@gmx.at>
2020-10-13 15:29:56 +0200

    tried something but defeated it

--- Basic/basic1.ts
+++ Basic/basic1.ts
@@ -2,6 +2,6 @@ let numbers : number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 for (let value of numbers){
 	for(let index in numbers){
-		console.log(`${Number(index) + 1} * ${value}`);
+		console.log(`${Number(index) + 1} * ${value} = `);
 	}
 }
